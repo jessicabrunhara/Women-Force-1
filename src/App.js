62d005d6c431b9c079ea52fc978caee7467d3cec
Milena Ferraz <milenaferraz@gmail.com>
2020-09-09 14:27:00 -0300

    merge-meninas

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import ControlledCarousel from './Carousel.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import WomenForceRouter from './routes'
 import Header from './components/header/header';
 import './assets/css/global.css'
-import Banner from './components/home/Banner'
-import Search from './components/home/Search'
 
 function App() {
   return (
     <div>         
       <Header />
-      <WomenForceRouter />
-      <Banner />
-      <section className="search">
-      <Search />
-      </section>
-      <section className="carousel">
-      <ControlledCarousel />
-      </section>
+      <WomenForceRouter />  
     </div>
   );
 }
