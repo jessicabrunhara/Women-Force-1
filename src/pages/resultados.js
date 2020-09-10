b88feb19507cad43ca2a08221c1f0582901f0c42
Evellyn <xe.uyemura@gmail.com>
2020-09-10 12:15:01 -0300

    card pagina resultados

--- src/pages/resultados.js
+++ src/pages/resultados.js
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import firebase from '../plugins/firebase'
+import { Card } from 'react-bootstrap'
 
 const Resultados = () => {
+  const [recomendation, setRecomendation] = useState([])
 
+  useEffect(() => {
+    firebase
+      .firestore()
+      .collection('avaliations')
+      .get()
+      .then((querySnapshot) => {
+        setRecomendation(
+          querySnapshot.docs
+          .map((i) => ({ ...i.data(), idDoc: i.id }))
+        )
+      })
+  }, [])
+  
   return (
-    <div></div>
+    <>
+      <h2>Recomendações</h2>
+      <ul>
+        <Card>
+          {recomendation.map((i, index) => {
+            return (
+              <li key={index}>
+                <Card.Header>
+                  Empresa
+                </Card.Header>
+                <Card.Body>
+                <Card.Text>
+                  <p>
+                  Nome: {i.name}
+                  <br /> 
+                  Primeira pergunta: {i.firstQuestion}
+                  </p>
+                </Card.Text>
+                </Card.Body>
+              </li>
+            );
+          })}
+        </Card>
+      </ul>
+    </>
   )
 }
+
 export default Resultados
