d7a2360bd0c615902468dcc990a83fea85d35cd9
Evellyn <xe.uyemura@gmail.com>
2020-09-11 10:08:42 -0300

    modificando a Readme

--- src/pages/avaliacao.js
+++ src/pages/avaliacao.js
@@ -3,11 +3,11 @@ import FormAvaliar from '../components/Avaliacao/avaliacao'
 
 
 const Avaliacao = () => {
-
   return (
     <div className='avaliation-wrapper'>
       <FormAvaliar />
     </div>
   )
+}
 
 export default Avaliacao;
